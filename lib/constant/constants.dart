0461780e7d3011568167bc334887487f60dfdc8d
Aman Verma <138030366+champacked@users.noreply.github.com>
2024-03-28 17:41:50 +0530

    Add files via upload

---
+++ lib/constant/constants.dart
@@ -0,0 +1,12 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEVDMUI0QzIyOTU5NkIzMTE5NWMzNTVCZmExMTQxMGFlYjcxNEM1MzkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4NDQzNDE3ODg5NiwibmFtZSI6IkxhbmRfcmVnaXN0cnkifQ.2q1zDiFGsD0l9FgtRE3_Eb6ThkBrGsjHYVslF7lZeuc';
+
+const String rpcUrl = "http://127.0.0.1:7545";
+
+const String contractAddress =
+    "0xa7eC8248b465DA953b9EBE3f6BBE5BeE9a562410"; //"0xbc6d9e15255533C19f1589A7e9fbB60440a539e0";
+
+const int chainId = 1337; // local ganache-1337
